b4fd8bff8f0c215c7b4d1afd53d1a06c0e9a5347
Mikolaj Snochowski <snochopl@gmail.com>
2020-03-23 22:34:28 +0100

    Mvp of showing each pupil

--- webpack-react/src/App.js
+++ webpack-react/src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Pupil from './components/Pupil';
+import styled from 'styled-components';
 
 const App = () => {
 
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost/e-dziennik-sbd/api/')
+      .then(response => response.json())
+      .then(value => {
+        try {
+          if (value.error) {
+            throw err;
+          }
+          setData(value);
+        } catch (err) {
+          console.error('Occured error', err);
+        }
+      })
+  }, []);
 
   return (
-    <div>
-      <h1>Hello admin</h1>
-    </div>
+    <Wrapper>
+      {
+        data.length > 0 ? data.map(pupil => <Pupil key={pupil.id} {...pupil} />) : 'Loading...'
+      }
+    </Wrapper>
   );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
