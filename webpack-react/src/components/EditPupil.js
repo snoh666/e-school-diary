330a3c8238f06e53daf4b4a9385a576d772716dd
Mikolaj Snochowski <snochopl@gmail.com>
2020-03-24 18:04:34 +0100

    Working user edition component

---
+++ webpack-react/src/components/EditPupil.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
+import {Redirect, Link} from 'react-router-dom';
+
+const EditPupil = ({router, usersData, setNeedUpdate}) => {
+
+  const [matchedPupil, setMatchedPupil] = useState({});
+  const [isSaved, setIsSaved] = useState(true);
+  // Grades states
+  const [mathGrades, setMathGrades] = useState('');
+  const [polishGrades, setPolishGrades] = useState('');
+  const [itGrades, setItGrades] = useState('');
+
+  useEffect(() => {
+    if (usersData.length > 0) {
+      setMatchedPupil(
+        usersData.filter(
+          pupil => pupil.id == Number(router.match.params.id),
+        )[0],
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(matchedPupil).length) {
+      const {math, polish, it} = matchedPupil.grades;
+      setMathGrades(math);
+      setPolishGrades(polish);
+      setItGrades(it);
+    }
+  }, [matchedPupil])
+
+
+  const sendForm = e => {
+    e.preventDefault();
+
+    fetch('http://localhost/e-dziennik-sbd/api/pupil/edit/', {method: 'POST', body: JSON.stringify({
+      'pupil_id': matchedPupil.id,
+      grades: {
+        math: mathGrades,
+        polish: polishGrades,
+        it: itGrades
+      }
+    })})
+      .then(response => response.json())
+      .then(value => {
+        try {
+          if (value.error) {
+            throw value.error;
+          } else if (value.message) {
+            setIsSaved(true);
+            setNeedUpdate(true);
+          }
+
+        } catch (err) {
+          console.error(err);
+        }
+      });
+  }
+
+  const changedIsSaved = () => setIsSaved(false);
+
+  return usersData.length ? (
+    <Wrapper>
+      <h2>Edit Pupil</h2>
+      <p>{matchedPupil.fullname}</p>
+      <form onSubmit={sendForm}>
+        <input
+          type='text'
+          name='math'
+          id='math'
+          onChange={e => {setMathGrades(e.target.value); changedIsSaved();}}
+          value={mathGrades}
+        />
+
+        <input
+          type='text'
+          name='polish'
+          onChange={e => {setPolishGrades(e.target.value); changedIsSaved();}}
+          value={polishGrades}
+        />
+
+        <input
+          type='text'
+          name='it'
+          onChange={e => {setItGrades(e.target.value); changedIsSaved();}}
+          value={itGrades}
+        />
+
+        {isSaved ? (
+          <button type='submit' disabled>
+            Save
+          </button>
+        ) : (
+          <button type='submit'>Save</button>
+        )}
+      </form>
+      <Link replace to='/'>
+        Go Back
+      </Link>
+    </Wrapper>
+  ) : (
+    <Redirect exact to='/' />
+  );
+};
+
+export default EditPupil;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+`;
